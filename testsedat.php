3004b93c8c5007f58ef8ac7a9e1cd9721937d7ea
sbasaran <sedatbasaran208@hotmail.com>
2018-12-04 09:29:51 +0300

    Update testsedat.php

--- testsedat.php
+++ testsedat.php
@@ -1,22 +1,32 @@
 <?php 
 
-function sqlGenerator($total, $constant){
-	//$total = 0;
-	//$constant = 1500;
+function sqlGeneratorNonDecreasingData($total, $constant){
 	$remained = $total%$constant;
 	$multiplier = floor($total/$constant);
-	echo "count:".$total."<br>";
-	echo "multiplier".$multiplier."<br>";
-	echo "remained".$remained."<br>";
+	//echo "count:".$total."<br>";
+	//echo "multiplier".$multiplier."<br>";
+	//echo "remained".$remained."<br>";
 	$sqlArr=array();
 	for($i=1;$i<=$multiplier;$i++){
-		$sqlArr[] = "rownum >= ". ((($i-1)==0)?0:($constant*($i-1)) )." and rownum < ". $constant*$i ."<br>";
+		$sqlArr[] = " R >= ". ((($i-1)==0)?0:($constant*($i-1)) )." and R < ". $constant*$i ;
 	}
 	//last one
-	$sqlArr[] =  "rownum >= ". ($constant*($multiplier)) ." and rownum <= ".(($constant*$multiplier)+$remained)."<br>";
+	$sqlArr[] =  " R >= ". ($constant*($multiplier)) ." and R <= ".(($constant*$multiplier)+$remained);
 	return $sqlArr;
 }
-
-print_r(sqlGenerator(10000,1000));
+function sqlGeneratorDecreasingData($total, $constant){
+	$remained = $total%$constant;
+	$multiplier = floor($total/$constant);
+	//echo "count:".$total."<br>";
+	//echo "multiplier".$multiplier."<br>";
+	//echo "remained".$remained."<br>";
+	$sqlArr=array();
+	for($i=1;$i<=$multiplier;$i++){ 
+		$sqlArr[] = " R >= 0 and R < ". $constant ;
+	}
+	//last one
+	$sqlArr[] =  " R >= 0 and R <= ".$remained;
+	return $sqlArr;
+}
 
 ?>
